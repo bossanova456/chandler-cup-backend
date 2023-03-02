dee500eeaef368794eb55fbb7e1ab5a8b2a84e9d
Taylor Andrews <88749086+bossanova456@users.noreply.github.com>
2023-03-02 15:12:09 -0500

    Dockerfile & GHA workflow

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const createError = require('http-errors');
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -11,10 +10,18 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+const cors=require("cors");
+const corsOptions = {
+   origin:'*', 
+   credentials:true,            // access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/matchups', matchupsRouter);
