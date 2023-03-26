1afd0c7813d8b8c4e710163b685f0db1e51cf7e8
Taylor Andrews <88749086+bossanova456@users.noreply.github.com>
2023-03-25 22:32:16 -0400

    Added get current year route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const { getCurrentSeasonYear } = require('../redis');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.sendStatus(418);
 });
+
+router.get('/year', function( req, res) {
+  getCurrentSeasonYear()
+    .then(year => {
+      if (year) res.send(year);
+      else res.sendStatus(404);
+    })
+})
 
 module.exports = router;
