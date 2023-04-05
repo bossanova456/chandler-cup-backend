1a54336b565c8e80264a15f224a98bcf424c0170
Taylor Andrews <88749086+bossanova456@users.noreply.github.com>
2023-04-04 23:20:04 -0400

    Reformatted matchup data

--- src/data/MatchupData.js
+++ src/data/MatchupData.js
@@ -1,91 +1,91 @@
 const matchupData = {
-	"01": [
-		{
+	"01": {
+		"0102": {
 			"favoredTeamId": "01",
 			"underdogTeamId": "02",
 			"line": 3,
-			"game_start_ts": "01-01-1970 00:00:00.000",
+			"game_start_ts": "01/01/2022 00:00:00",
 			"favoredScore": 0,
 			"underdogScore": 0,
 			"isFinal": false
 		},
-		{
+		"0304": {
 			"favoredTeamId": "03",
 			"underdogTeamId": "04",
 			"line": 3,
-			"game_start_ts": "01-01-1970 00:00:00.000",
+			"game_start_ts": "01/01/2022 00:00:00",
 			"favoredScore": 0,
 			"underdogScore": 0,
 			"isFinal": false
 		},
-		{
+		"0506": {
 			"favoredTeamId": "05",
 			"underdogTeamId": "06",
 			"line": 4,
-			"game_start_ts": "01-01-1970 00:00:00.000",
+			"game_start_ts": "01/01/2022 00:00:00",
 			"favoredScore": 0,
 			"underdogScore": 0,
 			"isFinal": false
 		},
-		{
+		"0708": {
 			"favoredTeamId": "07",
 			"underdogTeamId": "08",
 			"line": 3,
-			"game_start_ts": "01-01-1970 00:00:00.000",
+			"game_start_ts": "01/01/2022 00:00:00",
 			"favoredScore": 0,
 			"underdogScore": 0,
 			"isFinal": false
 		},
-		{
+		"0910": {
 			"favoredTeamId": "09",
 			"underdogTeamId": "10",
 			"line": 3,
-			"game_start_ts": "01-01-1970 00:00:00.000",
+			"game_start_ts": "01/02/2022 16:20:00",
 			"favoredScore": 0,
 			"underdogScore": 0,
 			"isFinal": false
 		},
-		{
+		"1112": {
 			"favoredTeamId": "11",
 			"underdogTeamId": "12",
 			"line": 3,
-			"game_start_ts": "01-01-1970 00:00:00.000",
+			"game_start_ts": "01/01/2022 00:00:00",
 			"favoredScore": 0,
 			"underdogScore": 0,
 			"isFinal": false
 		},
-		{
+		"1314": {
 			"favoredTeamId": "13",
 			"underdogTeamId": "14",
 			"line": 3,
-			"game_start_ts": "01-01-1970  00:00:00.000",
+			"game_start_ts": "01/01/2022 13:05:00",
 			"favoredScore": 0,
 			"underdogScore": 0,
 			"isFinal": false
 		}
-	],
-	"02": [
-		{
+	},
+	"02": {
+		"0304": {
 			"favoredTeamId": "03",
 			"underdogTeamId": "04",
 			"line": 3,
-			"game_start_ts": "01-01-1970 00:00:00.000",
+			"game_start_ts": "01/02/2022 16:20:00",
 			"favoredScore": 0,
 			"underdogScore": 0,
 			"isFinal": false
 		}
-	],
-	"03": [
-		{
+	},
+	"03": {
+		"0301": {
 			"favoredTeamId": "03",
 			"underdogTeamId": "01",
 			"line": 3,
-			"game_start_ts": "01-01-1970 00:00:00.000",
+			"game_start_ts": "01/01/2022 00:00:00",
 			"favoredScore": 0,
 			"underdogScore": 0,
 			"isFinal": false
 		}
-	]
+	}
 };
 
 module.exports = {
